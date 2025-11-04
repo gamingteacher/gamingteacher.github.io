b48829a7fe0c26de652c0d1f3cc8a8596c91867c
gamingteacher <krlloz.h@gmail.com>
2025-11-04 09:38:49 -0300

    Initial release

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./script.js"],
+  theme: {
+    extend: {
+      colors: {
+        'red': '#D94C50',
+        'grey': '#343434',
+        'white': '#F2F2F2',
+        'green': '#2ABD51',
+      }
+    },
+  },
+  plugins: [],
+}
